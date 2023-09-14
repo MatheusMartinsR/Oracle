d6362ea0a8857dc520b15bd1ca0ef04aaf9d62c3
logonrmlocal <logonrmlocal@PA407MICRO22.fiap.com.br>
2023-09-14 20:54:04 -0300

    Criando outro arquivo de exception

---
+++ exception2.sql
@@ -0,0 +1,7 @@
+select sum (1+5) from dual;
+select avg(codigo_cargo) from cargo;
+select count(1) from cargo;
+select min(codigo_cargo) from cargo;
+select min(codigo_cargo) from carg;
+
+select * from cargo
