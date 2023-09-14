6e39e3509f8e7dc0c4429a9b8b78818ff3405a0f
logonrmlocal <logonrmlocal@PA407MICRO22.fiap.com.br>
2023-09-14 20:17:31 -0300

    Atualizando a função

--- procedures.sql
+++ procedures.sql
@@ -2,6 +2,7 @@ set serveroutput on;
 
 
 create or replace procedure procedure_fgts is 
+v_valor number;
 begin 
 v_valor := calcula_FGTS(5000);
 dbms_output.put_line('O valor do FGTS é: ' || to_char(v_valor));
