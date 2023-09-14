3669309a994fcfcf48b04d3ad1f17a0d19ab5305
logonrmlocal <logonrmlocal@PA407MICRO22.fiap.com.br>
2023-09-14 20:13:01 -0300

    Criando uma função

--- procedures.sql
+++ procedures.sql
@@ -1,1 +1,10 @@
-select to_char(sysdate, 'dd-mm-yyyy hh24:mi:ss') data from dual;
+set serveroutput on;
+
+
+create or replace procedure procedure_fgts is 
+begin 
+v_valor := calcula_FGTS(5000);
+dbms_output.put_line('O valor do FGTS é: ' || to_char(v_valor));
+end procedure_FGTS;
+
+exec procedure_FGTS();
